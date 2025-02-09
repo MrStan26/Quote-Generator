96405a222736e9332851776e80ba117da227a262
MrStan26 <nnaemekastanlo@gmail.com>
2025-02-09 09:34:43 -0800

    first commit

---
+++ quotes.js
@@ -0,0 +1,74 @@
+const localQuotes = [
+    {
+        text: 'Genius is one percent inspiration and ninety-nine percent perspiration',
+        author: 'Thomas Edison',
+    },
+    {
+        text: 'You can observe a lot just by watching.',
+        author: 'Yogi Berra',
+    },
+    {
+        text: 'A house divided against itself cannot stand.',
+        author: 'Abraham Lincoln',
+    },
+    {
+        text: 'Difficulties increases the nearer we get to the goal.',
+        author: 'Johann wolfgang von Goethe',
+    },
+    {
+        text: 'Think how hard physics would be if particles could think.',
+        author: 'Murray gell-Mann',
+    },
+    {
+        text: "Love is the flower you've got to let grow.",
+        author: 'John Lennon',
+    },
+    {
+        text: "Don't wait. The time will never be just right.",
+        author: '',
+    },
+    {
+        text: 'Time is the wisest counsellor of all.',
+        author: 'Pericles',
+    },
+    {
+        text: 'change in all things is sweet.',
+        author: 'Aristotle',
+    },
+    {
+        text: 'life is movement-we breathe, we eat, we walk, we move!',
+        author: 'John Pierrakos',
+    },
+    {
+        text: "Argue for your limitations, and sure enough they're yours.",
+        author: 'Richard Bach',
+    },
+    {
+        text: 'No one can make you feel inferrior without your consent.',
+        author: 'Eleanor Roosevelt',
+    },
+    {
+        text: 'Luck is what happens when preparation meets opprotunity.',
+        author: '',
+    },
+    {
+        text: 'He who feeds you, controls you',
+        author: 'Thomas Sankara',
+    },
+    {
+        text: 'You cannot carry out fundamental change without a certain amount of madness',
+        author: 'Thomas Sankara',
+    },
+    {
+        text: "Those who come with wheat, millet, corn or milk, they're not helping us. We believed that food aid should include tools and resources to help people grow their own food.",
+        author: 'Thomas sankara',
+    },
+    {
+        text: 'Life is like a riding bicycle. To keep your balance, you must keep moving.',
+        author: '',
+    },
+    {
+        text: 'Not all those who wander are lost.',
+        author: '',
+    }
+]
